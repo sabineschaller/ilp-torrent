eb3217ef08ecfc976f97c19060f69d86131e9665
Sabine <sabine.bertram@mailbox.org>
2020-08-14 16:28:20 +0200

    feat: server implementation

---
+++ src/server/index.js
@@ -0,0 +1,36 @@
+const WebTorrent = require('../../lib/ilp-webtorrent-hybrid')
+const Express = require('express')
+const fileUpload = require('express-fileupload')
+
+const client = new WebTorrent()
+const app = new Express()
+
+app.use(fileUpload())
+
+app.post('/seed', (req, res) => {
+  const extension = req.files.file.name.split('.').slice(-1)
+  const info = {
+    name: req.body.name ? `${req.body.name}.${extension}` : req.files.file.name,
+    private: true,
+    announce: 'ws://localhost:8000',
+    paymentRequired: true,
+    license: {
+      paymentPointer: req.body.paymentPointer,
+      verifier: req.body.verifier,
+      amount: req.body.amount,
+      asset: req.body.asset
+    },
+    requestId: ''
+  }
+  const buf = req.files.file.data
+  buf.name = info.name
+  client.seed(buf, info)
+  client.on('torrent', function (torrent) {
+    console.log('Client is seeding ' + torrent.magnetURI)
+    return res.send(torrent.magnetURI)
+  })
+})
+
+app.listen(8081, () => {
+  console.log('Listening on port 8081...')
+})
